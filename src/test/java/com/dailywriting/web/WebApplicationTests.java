9e5ea5f0de6bfa40d5d115837c87d18232941768
HaeSung Lee <haesung.dev@gmail.com>
2019-12-24 01:02:56 +0900

    init project

---
+++ src/test/java/com/dailywriting/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailywriting.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
