beba4ce1d26ceffcfdfc00b96ecb48d0787aeb4b
HaeSung <haesung.dev@gmail.com>
2020-06-23 00:07:39 +0900

    fix: change user id geneartion strategy
    - identity => Auto

--- src/main/java/com/dailywriting/web/user/User.java
+++ src/main/java/com/dailywriting/web/user/User.java
@@ -18,7 +18,7 @@ import javax.validation.Constraint;
 )
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "user_id")
     private long id;
 
